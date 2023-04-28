d17ecaf52c27a2b75802f6b4e92c7a8a915ba37a
Vilde Ung <67263002+ungvilde@users.noreply.github.com>
2023-04-28 13:33:33 +0200

    working simple rnn class

--- simple_rnn.py
+++ simple_rnn.py
@@ -1,73 +1,35 @@
-##################### SIMPLE RECURRENT NEURAL NETWORK ##########################
-import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, SimpleRNN, GRU, Activation, Dropout
+from tensorflow.keras.layers import Dense, SimpleRNN, Dropout
 
-class SimpleRNNRegression(object):
+class SimpleRNNRegression:
 
-    """
-    Class for the simple recurrent neural network decoder
-    Parameters
-    ----------
-    units: integer, optional, default 400
-        Number of hidden units in each layer
-    dropout: decimal, optional, default 0
-        Proportion of units that get dropped out
-    num_epochs: integer, optional, default 10
-        Number of epochs used for training
-    verbose: binary, optional, default=0
-        Whether to show progress of the fit after each epoch
-    """
-
-    def __init__(self,units=400,dropout=0,num_epochs=10,verbose=0):
+    def __init__(self, units=100, num_epochs=10, verbose=1, activation="relu", optimizer="adam"):
          self.units=units
          self.dropout=dropout
          self.num_epochs=num_epochs
          self.verbose=verbose
+         self.activation=activation
+         self.optimizer=optimizer
 
+    def fit(self, X_train, y_train):
 
-    def fit(self,X_train,y_train):
-
-        """
-        Train SimpleRNN Decoder
-        Parameters
-        ----------
-        X_train: numpy 3d array of shape [n_samples,n_time_bins,n_neurons]
-            This is the neural data.
-            See example file for an example of how to format the neural data correctly
-        y_train: numpy 2d array of shape [n_samples, n_outputs]
-            This is the outputs that are being predicted
-        """
-
-        model=Sequential() #Declare model
-        #Add recurrent layer
-        model.add(SimpleRNN(self.units,input_shape=(X_train.shape[1],X_train.shape[2]),dropout=self.dropout,recurrent_dropout=self.dropout,activation='relu')) #Within recurrent layer, include dropout
-        if self.dropout!=0: model.add(Dropout(self.dropout)) #Dropout some units (recurrent layer output units)
-
-        #Add dense connections to output layer
+        model=Sequential() 
+        model.add(
+            SimpleRNN(self.units, input_shape=(X_train.shape[1], X_train.shape[2]),
+            recurrent_dropout=self.dropout,
+            activation=self.activation)
+            ) 
+            
         model.add(Dense(y_train.shape[1]))
 
-        #Fit model (and set fitting parameters)
-        model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy']) #Set loss function and optimizer
-        model.fit(X_train,y_train,epochs=self.num_epochs,verbose=self.verbose) #Fit the model
-        self.model=model
+        model.compile(loss='mse', optimizer=self.optimizer, metrics=['accuracy'])
+        model.fit(X_train, y_train, epochs=self.num_epochs, verbose=self.verbose)
+        self.model = model
 
 
-    def predict(self,X_test):
+    def predict(self, X_test):
+        y_pred = self.model.predict(X_test)
 
-        """
-        Predict outcomes using trained SimpleRNN Decoder
-        Parameters
-        ----------
-        X_test: numpy 3d array of shape [n_samples,n_time_bins,n_neurons]
-            This is the neural data being used to predict outputs.
-        Returns
-        -------
-        y_test_predicted: numpy 2d array of shape [n_samples,n_outputs]
-            The predicted outputs
-        """
-
-        y_test_predicted = self.model.predict(X_test) #Make predictions
-        return y_test_predicted
+        return y_pred
 
 
