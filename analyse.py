13693741e89cf470ce53e13f7ea02e19e1602486
Vilde Ung <67263002+ungvilde@users.noreply.github.com>
2023-04-28 13:33:50 +0200

    simple test analysis of m1 data

--- analyse.py
+++ analyse.py
@@ -1,15 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
 from simple_rnn import SimpleRNNRegression
 
 from data_preprocessing import *
+from metrics import *
 
-datafile = 's1'
+datafile = 'm1'
 
 with open('data/' + datafile + '_neural_data.pickle','rb') as f:
     neural_data, vels_binned=pickle.load(f)
@@ -21,7 +19,16 @@ bins_after=0
 X=get_spikes_with_history(neural_data, bins_before, bins_after, bins_current)
 y=vels_binned
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
+X = X[(bins_before+2):] # first 14 slices do not have any values because of history effects
+y = y[(bins_before+2):]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=543)
+
+# Scale the data
+X_train_mean=np.nanmean(X_train,axis=0) #Mean of training data
+X_train_std=np.nanstd(X_train,axis=0) #Stdev of training data
+X_train=(X_train-X_train_mean)/X_train_std #Z-score training data
+X_test=(X_test-X_train_mean)/X_train_std #Preprocess testing data in same manner as training data
 
 print("X_train shape=",X_train.shape)
 print("y_train shape=",y_train.shape)
@@ -29,6 +36,13 @@ print("y_train shape=",y_train.shape)
 print("X_test shape=",X_test.shape)
 print("y_test shape=",y_test.shape)
 
-rnn = SimpleRNNRegression()
+rnn = SimpleRNNRegression(units=100)
 
 rnn.fit(X_train=X_train, y_train=y_train)
+y_pred = rnn.predict(X_test=X_test)
+
+print("y_pred shape = ", y_pred.shape)
+
+R2 = r2_score(y_true = y_test, y_pred = y_pred)
+
+print("R2 = ", R2)
