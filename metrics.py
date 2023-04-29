306dadec65b9d5428d64e2da632f4270bd3ce64c
Vilde Ung <67263002+ungvilde@users.noreply.github.com>
2023-04-29 13:28:33 +0200

    naive bayes kind of working

--- metrics.py
+++ metrics.py
@@ -9,3 +9,25 @@ def r2_score(y_true, y_pred):
 
 def mean_squared_error(y_true, y_pred):
     return np.average( (np.ravel(y_pred) - np.ravel(y_true))**2 )
+
+def get_R2(y_test,y_test_pred):
+
+    """
+    Function to get R2
+    Parameters
+    ----------
+    y_test - the true outputs (a matrix of size number of examples x number of outputs)
+    y_test_pred - the predicted outputs (a matrix of size number of examples x number of outputs)
+    Returns
+    -------
+    R2_array: An array of R2s for each output
+    """
+
+    R2_list=[] #Initialize a list that will contain the R2s for all the outputs
+    for i in range(y_test.shape[1]): #Loop through outputs
+        #Compute R2 for each output
+        y_mean=np.mean(y_test[:,i])
+        R2=1-np.sum((y_test_pred[:,i]-y_test[:,i])**2)/np.sum((y_test[:,i]-y_mean)**2)
+        R2_list.append(R2) #Append R2 of this output to the list
+    R2_array=np.array(R2_list)
+    return R2_array #Return an array of R2s
--- prepare_data.py
+++ prepare_data.py
@@ -5,16 +5,15 @@ import pickle
 
 
 for datafile in ['hc', 'm1', 's1']:
-#for data in ['data/m1_data_raw.mat']:
     data=io.loadmat('data/'+ datafile + '_data_raw.mat')
     spike_times=data['spike_times'] 
-    print(datafile, data.keys())
     dt=.05 #Size of time bins (in seconds)
 
     # spike_times=data['spike_times'] #Load spike times of all neurons
     if datafile in ['m1', 's1']:
         vels=data['vels'] #Load x and y velocities
         vel_times=data['vel_times'] #Load times at which velocities were recorded
+
         t_start=vel_times[0] #Time to start extracting data - here the first time velocity was recorded
         t_end=vel_times[-1]
         downsample_factor=1
@@ -25,8 +24,6 @@ for datafile in ['hc', 'm1', 's1']:
         for i in range(spike_times.shape[0]):
             spike_times[i]=np.squeeze(spike_times[i])
 
-
-        #### FORMAT OUTPUT ####
         #Bin output (velocity) data using "bin_output" function
         vels_binned=bin_output(vels, vel_times, dt, t_start, t_end, downsample_factor)
 
@@ -36,29 +33,28 @@ for datafile in ['hc', 'm1', 's1']:
 
         with open('data/'+datafile+'_neural_data.pickle','wb') as f:
             pickle.dump([neural_data, vels_binned],f)
-    # else:
-    #     pos=data['pos']
-    #     pos_times=data['pos_times']
+    else:
+        pos=data['pos']
+        pos_times=data['pos_times'][0] 
+        dt=.2
 
-    #     t_start=pos_times[0] #Time to start extracting data - here the first time velocity was recorded
-    #     t_end=pos_times[-1]
-    #     downsample_factor=1
+        t_start=pos_times[0] #Time to start extracting data - here the first time velocity was recorded
+        t_end=5608
+        downsample_factor=1
 
-    #     #When loading the Matlab cell "spike_times", Python puts it in a format with an extra unnecessary dimension
-    #     #First, we will put spike_times in a cleaner format: an array of arrays
-    #     spike_times=np.squeeze(spike_times)
-    #     for i in range(spike_times.shape[0]):
-    #         spike_times[i]=np.squeeze(spike_times[i])
+        #When loading the Matlab cell "spike_times", Python puts it in a format with an extra unnecessary dimension
+        #First, we will put spike_times in a cleaner format: an array of arrays
+        spike_times=np.squeeze(spike_times)
+        for i in range(spike_times.shape[0]):
+            spike_times[i]=np.squeeze(spike_times[i])
 
-    #     #### FORMAT OUTPUT ####
-    #     #Bin output (velocity) data using "bin_output" function
-    #     pos_binned=bin_output(pos, pos_times, dt, t_start, t_end, downsample_factor)
+        #Bin output (position) data using "bin_output" function
+        pos_binned=bin_output(pos, pos_times, dt, t_start, t_end, downsample_factor)
 
-    #     #### FORMAT INPUT ####
-    #     #Bin neural data using "bin_spikes" function
-    #     neural_data=bin_spikes(spike_times, dt, t_start, t_end)
+        #Bin neural data using "bin_spikes" function
+        neural_data=bin_spikes(spike_times, dt, t_start, t_end)
 
-    #     data_folder='data/' 
+        data_folder='data/' 
 
-    #     with open(data_folder+'_neural_data.pickle','wb') as f:
-    #         pickle.dump([neural_data, vels_binned],f)
+        with open('data/'+datafile+'_neural_data.pickle','wb') as f:
+            pickle.dump([neural_data, pos_binned], f)
